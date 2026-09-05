cda35e0117e7219976e642ed0c22e6ad070e9df5
Ram Charan <ramcharan.c2211@gmail.com>
2026-09-05 12:27:35 +0530

    Fix booking model import case

--- Backend/routes/bookingRoutes.js
+++ Backend/routes/bookingRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const Booking = require("../models/Booking");
+const Booking = require("../models/booking");
 const authMiddleware = require("../middleware/auth");
 
 // ==========================================
